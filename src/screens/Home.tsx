61b1255ae806687d2a4ca5e883a84d0ad4659608
Igor Serafim <igorserafim2015@hotmail.com>
2022-12-16 19:00:29 -0300

    started styles screens privates

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,26 @@
-import { Center, Text } from 'native-base'
+import { Group } from '@components/Group'
+import { HomeHeader } from '@components/HomeHeader'
+import { HStack, VStack } from 'native-base'
+import { useState } from 'react'
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState('costa')
+
   return (
-    <Center flex={1}>
-      <Text>Home</Text>
-    </Center>
+    <VStack flex={1}>
+      <HomeHeader />
+      <HStack>
+        <Group
+          name="costa"
+          isActive={groupSelected === 'costa'}
+          onPress={() => setGroupSelected('costa')}
+        />
+        <Group
+          name="ombro"
+          isActive={groupSelected === 'ombro'}
+          onPress={() => setGroupSelected('ombro')}
+        />
+      </HStack>
+    </VStack>
   )
 }
