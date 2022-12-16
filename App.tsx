a88b5c9ac15e90165d11be02a27f5b1868bfa5ce
Igor Serafim <igor.serafim@nolifretes.com.br>
2022-12-15 22:34:30 -0300

    added native-base

--- App.tsx
+++ App.tsx
@@ -1,13 +1,25 @@
 /* eslint-disable camelcase */
-import { Text, View } from 'react-native'
+import { StatusBar, View } from 'react-native'
+import { NativeBaseProvider } from 'native-base'
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
+import { THEME } from 'src/theme'
+import { Loading } from '@components/Loading'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
-  return <View>{fontsLoaded ? <Text>Hello World!</Text> : <View />}</View>
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <View /> : <Loading />}
+    </NativeBaseProvider>
+  )
 }
